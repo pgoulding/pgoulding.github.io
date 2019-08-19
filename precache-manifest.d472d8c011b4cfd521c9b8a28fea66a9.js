0d973c742d8e81ad0ada65fc64210c8f751ac4c5
Patrick Goulding <patrickgoulding@gmail.com>
2019-08-19 14:49:16 -0600

    chnage build directory

---
+++ precache-manifest.d472d8c011b4cfd521c9b8a28fea66a9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627448e1d314f85fd65296bb8acbfcf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fc543f9195089bcbe5d",
+    "url": "/static/css/main.f6536885.chunk.css"
+  },
+  {
+    "revision": "c6f141dcc4abd666c4a0",
+    "url": "/static/js/2.972d842a.chunk.js"
+  },
+  {
+    "revision": "0fc543f9195089bcbe5d",
+    "url": "/static/js/main.019dea3b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a80ea6df229936aff32f879d985098a6",
+    "url": "/static/media/PatrickGouldingResume.a80ea6df.pdf"
+  },
+  {
+    "revision": "c1250942bc9cec90812f01d640f2f9b8",
+    "url": "/static/media/blog.c1250942.jpeg"
+  },
+  {
+    "revision": "4f594887a4f25b51cf8ba27a57f1c84a",
+    "url": "/static/media/code.4f594887.png"
+  },
+  {
+    "revision": "b47c46a9ba12bf108b033dc877fe47b2",
+    "url": "/static/media/giardanera.b47c46a9.jpg"
+  },
+  {
+    "revision": "3dc2c7dcaa77b0ebae9f99ecf50d4ead",
+    "url": "/static/media/github-logo.3dc2c7dc.png"
+  },
+  {
+    "revision": "fd0d5546fdbdc85c76c4372a0d51f1bc",
+    "url": "/static/media/linkedin.fd0d5546.png"
+  },
+  {
+    "revision": "fe24bb326aa69a28f38e5cddc71a7fb8",
+    "url": "/static/media/profile-pic.fe24bb32.jpg"
+  },
+  {
+    "revision": "df3f42ddff50212668299d59da0043dd",
+    "url": "/static/media/resume-pic.df3f42dd.png"
+  },
+  {
+    "revision": "0251d8ee95aa6d1f3400faa3b46b4bcf",
+    "url": "/static/media/twitter.0251d8ee.png"
+  },
+  {
+    "revision": "fb3ac3d6eff34a9efdc73f3343c84321",
+    "url": "/static/media/vtx1300.fb3ac3d6.jpg"
+  }
+]);
---
+++ test-env/src/data/portfolio.js
@@ -0,0 +1,164 @@
+import career_img from '../images/resume-pic.png'
+import portfolio_img from '../images/code.png'
+import my_story_img from '../images/profile-pic.jpg'
+import blog_img from '../images/blog.jpeg'
+import recipe_img from '../images/giardanera.jpg'
+import motorcycle_img from '../images/vtx1300.jpg'
+import resume from '../data/PatrickGouldingResume.pdf'
+
+export const homePageLinks = [
+  {
+    title:'Career',
+    page_url:'/career',
+    image_url: career_img,
+    description: 'PlaceHolder for Career Description',
+    links: [
+      { 
+        title: 'Resume PDF ⬇️', 
+        url:resume
+      }
+    ],
+    id:1,
+  },
+  {
+    title: 'Portfolio',
+    page_url: '/portfolio',
+    image_url: portfolio_img,
+    description: 'Placeholder for Portfolio Descirption',
+    links: [
+      { 
+        title: 'Cocktails and Dreams (React, Router, Redux, SCSS)', 
+        url:'https://github.com/pgoulding/coctail-recipes-app'
+      },
+      {
+        title:'Netflix 2.Woah(React, Router, Redux, SCSS)',
+        url:'https://github.com/pgoulding/Netflix-2.Woah'
+      },
+      {
+        title: 'Light Side : Dark Side (React, Router, SCSS)',
+        url: 'https://github.com/pgoulding/lightside'
+      },
+      {
+        title: 'Turing Year Book (React, Router, SCSS)',
+        url: 'https://github.com/pgoulding/turing-yearbook'
+      },
+      {
+        title: 'Overlook Hotel (jQuery,SCSS)',
+        url: 'https://github.com/pgoulding/overlook'
+      },
+      {
+        title: 'Miami Feud (jQuery, SCSS)',
+        url: 'https://github.com/pgoulding/Miami-Feud'
+      },
+      {
+        title: 'Flash Cards: Javascript Edition (Javascript ES6+, Mocha/Chai)',
+        url: 'https://github.com/pgoulding/javascript-flashcards'
+      },
+      {
+        title: 'Check Yo Self (Javascript ES5/ES6)',
+        url: 'https://github.com/pgoulding/check-yo-self'
+      },
+      {
+        title:'IdeaBox (Javascript ES5)',
+        url: 'https://github.com/pgoulding/idea-box'
+      },
+      {
+        title:'Number Guesser (Javascript ES5)',
+        url: 'https://github.com/pgoulding/Number-Guesser-Doubles'
+      }
+  ],
+    id:2,
+  },
+  {
+    title: 'My Story',
+    page_url:'/my_story',
+    image_url: my_story_img,
+    description: 'Placeholder for My Story Description',
+    links: [
+      {
+        title: '', 
+        url:''
+      }
+    ],
+    id:3,
+  },
+  {
+    title: 'Blog Posts',
+    page_url:'/blog',
+    image_url: blog_img,
+    description: 'All my blog posts so far!',
+    links: [
+      {
+        title: 'What the Scope - Medium', 
+        url:'https://medium.com/@patrickgoulding/what-the-scope-7c6afba0f265'
+      }
+    ],
+    id:4,
+  },
+  {
+    title: 'Recipes',
+    page_url:'/recipes',
+    image_url: recipe_img,
+    description: 'I love to cook and trying out new recipes is my favorite way to relax and unwind. I do a lot of pickling/canning/preserving as well as making my own hot sauces, and infusions. I hope you\'ll try out a few!',
+    links: [
+      {
+        title: 'Strawberry basil infused vodka with lemonade', 
+        url:'/recipes/strawberry-basil-lemonade'
+      },
+      {
+        title: 'Spicy Pickled Eggs',
+        url: '/recipes/pickled-eggs'
+      },
+      {
+        title: 'Hot Italian Giardinera',
+        url: '/recipes/giardinera'
+      },
+      {
+        title: 'Dill Pickles',
+        url: '/recipes/dill-pickles'
+      },
+    ],
+    id:5,
+  },
+  {
+    title: 'Hobbies',
+    page_url: '/hobbies',
+    image_url: motorcycle_img,
+    description: 'I love to go camping, ride my motorcycle through the mountains, bike riding around town, creating new recipes, and snowboarding in the winter, checkout some of my photos!',
+    links: [
+      {
+        title: 'Camping outside of Meredith, CO', 
+        url:'https://www.instagram.com/p/ByqWR4lgy75I0wBAtB5HQv5RxVETQCmqhEU2t00/'
+      }
+    ],
+    id: 6,
+  }
+]
+
+
+export const recipes = [
+  {
+    title: 'Strawberry basil infused vodka with lemonade',
+    page_url: '/recipes/strawberry-basil-lemonade',
+    image_url:'',
+    description:''
+  },
+  {
+    title: 'Spicy Pickled Eggs',
+    page_url: '/recipes/pickled-eggs',
+    image_url: '',
+    description: ''
+  },
+  {
+    title: 'Hot Italian Giardinera',
+    page_url: '/recipes/giardinera',
+    image_url: '',
+    description: ''
+  },
+  {
+    title: 'Dill Pickles',
+    page_url: '/recipes/dill-pickles',
+    image_url: '',
+    description: ''
+  },
+]
