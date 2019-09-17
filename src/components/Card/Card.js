6f825dbec4d25f8f53a96ddb805cb4f2734fc680
Patrick Goulding <patrickgoulding@gmail.com>
2019-09-16 23:17:25 -0600

    Add new resume, and change portfolio info, WIP add IG script

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,13 +4,23 @@ import  { Link } from 'react-browser-router'
 
 const Card = ({info}) => {
 
+  const makeScript = () => {
+    const script = document.createElement("script");
+    script.src = "https://www.instagram.com/embed.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
   return (
+    <>
+    {makeScript}
     <div className='card'>
       <img src={info.image_url} alt={info.title && ' image'}/>
       <Link to={info.page_url}>
         <h3>{info.title}</h3>
       </Link>
     </div>
+    </>
   )
 }
 
